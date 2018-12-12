2a6fc288be1bb5f78b26ca1563b427058214d0dc
lam <lam009@qq.com>
2018-12-12 18:16:32 +0800

    - 低版本（6.0以下）兼容vue的es6语法； - 回退键优化； - 断网重连设置回到当前首页，而不是去到登录界面； - 初次加载的用户交互； - 清除部分冗余代码和资源文件。

--- fanwehybridweb_v15/src/main/java/com/szruito/goldfields/activity/MainActivity.java
+++ fanwehybridweb_v15/src/main/java/com/szruito/goldfields/activity/MainActivity.java
@@ -445,17 +445,16 @@ public class MainActivity extends BaseActivity implements OnCropBitmapListner {
                 oks.disableSSOWhenAuthorize();
 
                 // title标题，微信、QQ和QQ空间等平台使用
-                oks.setTitle("分享至");
+                oks.setTitle("黄金原野，专为你自己打造的区块链价值平台！");
                 // titleUrl QQ和QQ空间跳转链接
-                oks.setTitleUrl("http://sharesdk.cn");
+                oks.setTitleUrl("https://fir.im/goldfields");
                 // text是分享文本，所有平台都需要这个字段
-                oks.setText("我是分享文本");
+                oks.setText("我正在使用《黄金原野APP》，快来跟我一起使用吧");
                 // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
                 oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
                 // url在微信、微博，Facebook等平台中使用
-                oks.setUrl("http://sharesdk.cn");
-                // comment是我对这条分享的评论，仅在人人网使用
-                oks.setComment("我是测试评论文本");
+                oks.setUrl("https://fir.im/goldfields");
+
                 // 启动分享GUI
                 oks.show(this);
                 break;
